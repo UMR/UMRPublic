e182ec708e2dfff7dfe14a0d3698bb746299fea3
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2018-07-04 15:30:25 +0600

    commit 2

--- UMRPublic/Job.aspx.designer.cs
+++ UMRPublic/Job.aspx.designer.cs
@@ -13,12 +13,21 @@ namespace UMRPublic {
     public partial class Job {
         
         /// <summary>
-        /// form1 control.
+        /// HyperLink1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// lstJobs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView lstJobs;
     }
 }
