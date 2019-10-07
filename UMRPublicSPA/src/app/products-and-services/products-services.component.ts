f6912c724cdaa36be47f870112730e2eb668834e
rraquib <reza.raquib@gmail.com>
2019-10-07 21:04:03 +0600

    2019-10-07
    day end

--- UMRPublicSPA/src/app/products-and-services/products-services.component.ts
+++ UMRPublicSPA/src/app/products-and-services/products-services.component.ts
@@ -1,7 +1,20 @@
-﻿import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { resourceServerUrl } from '../common/constants/auth-keys';
 @Component({
     templateUrl: './products-services.component.html'
 })
 export class ProductsServicesComponent{
-
+  umiDomain: string;
+  umrReqDomain: string;
+  ngOnInit() {
+    if (resourceServerUrl.includes('universalmedicalrecord.com')) {
+      this.umiDomain = 'http://universalmedicalrecord.com/Universal%20Medical%20Informatics/index.html';
+      this.umrReqDomain = 'http://universalmedicalrecord.com/UMR%20Recruitment%20Services/index.html';
+    }
+    else //if (resourceServerUrl.includes('umrtest.com')) 
+    {
+      this.umiDomain = 'http://umrtest.com/Universal%20Medical%20Informatics/index.html';
+      this.umrReqDomain = 'http://umrtest.com/UMR%20Recruitment%20Services/index.html';
+    }
+  }
 }
