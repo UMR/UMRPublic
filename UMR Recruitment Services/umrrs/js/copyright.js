481f14450dd8d04f9decb08923300329ee150a1a
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2020-03-27 12:43:31 +0600

    Added Facebook

--- UMR Recruitment Services/umrrs/js/copyright.js
+++ UMR Recruitment Services/umrrs/js/copyright.js
@@ -1,15 +1,15 @@
 ﻿$(document).ready(function () {
-    $('.copyright')[0].innerHTML = 'Copyright © 2019-' + new Date().getFullYear() + ' Universal Medical Informatics - All Rights Reserved<br/>';
+    $('.copyright')[0].innerHTML = 'Copyright © 2019-' + new Date().getFullYear() + ' Universal Medical Informatics - All Rights Reserved. Visit us on <a  style="color: #007bff !important;" href="https://www.facebook.com/Universal-Medical-Record-106157847529137/">Facebook</a><br/>';
 	if ($('.copyrightSpan')[0]) {
         $('.copyrightSpan')[0].innerHTML = new Date().getFullYear();
     }
-    $(".owl-carousel").owlCarousel({
-        autoPlay: false,
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
-        navigation: true,
-        pagination: false,
-        responsive: true
-    });
+    //$(".owl-carousel").owlCarousel({
+    //    autoPlay: false,
+    //    items: 4,
+    //    itemsDesktop: [1199, 3],
+    //    itemsDesktopSmall: [979, 3],
+    //    navigation: true,
+    //    pagination: false,
+    //    responsive: true
+    //});
 });
