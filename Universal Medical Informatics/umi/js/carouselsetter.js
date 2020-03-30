c048a6fb8c1f232e08b089d72377f3d816776236
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2020-03-30 15:58:15 +0600

    css fixes

---
+++ Universal Medical Informatics/umi/js/carouselsetter.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoPlay: false,
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        navigation: true,
+        pagination: false,
+        responsive: true
+    });
+});
--- Universal Medical Informatics/umi/js/copyright.js
+++ Universal Medical Informatics/umi/js/copyright.js
@@ -3,13 +3,4 @@
 	if ($('.copyrightSpan')[0]) {
         $('.copyrightSpan')[0].innerHTML = new Date().getFullYear();
     }
-    $(".owl-carousel").owlCarousel({
-        autoPlay: false,
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
-        navigation: true,
-        pagination: false,
-        responsive: true
-    });
 });
