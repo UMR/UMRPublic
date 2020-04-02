d5f020e6038c19e1e13aea7cf9aad6b98c8317fb
Sohel Rana <sohel606@hotmail.com>
2020-04-02 20:47:47 +0600

    fb link add

--- UMR Recruitment Services/umrrs/js/copyright.js
+++ UMR Recruitment Services/umrrs/js/copyright.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
-    $('.copyright')[0].innerHTML = 'Copyright © 2019-' + new Date().getFullYear() + ' Universal Medical Informatics - All Rights Reserved. Visit us on <a  style="color: #007bff !important;" href="https://www.facebook.com/Universal-Medical-Record-106157847529137/">Facebook</a><br/>';
-	if ($('.copyrightSpan')[0]) {
+    $('.copyright')[0].innerHTML = 'Copyright © 2019-' + new Date().getFullYear() + ' Universal Medical Informatics - All Rights Reserved.<br/> Visit us on: <a style="color: #4267b2 !important;font-size: 24px;padding:0 10px" href = "https://www.facebook.com/UMR-Recruitment-Services-112027723783186/" > <i class="fa fa-facebook"></i></a><a style="color: #1da0f2 !important;font-size: 24px;padding:0 10px" href = "" > <i class="fa fa-twitter"></i></a><a style="color: #0E76A8 !important;font-size: 24px;padding:0 10px" href = "" > <i class="fa fa-linkedin"></i></a ><a style="color: #1da0f2 !important;font-size: 24px;padding:0 10px" href = "" > <i  class="fa fa-google-plus"></i></a > <br />';
+    if ($('.copyrightSpan')[0]) {
         $('.copyrightSpan')[0].innerHTML = new Date().getFullYear();
     }
     //$(".owl-carousel").owlCarousel({
