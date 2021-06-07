b8ec0fb07ca3c4f99188994a819612dcc4e9f177
Kaptanael <arzaman@ael-bd.com>
2021-06-07 18:22:16 +0600

    commit

---
+++ UMRPublicAPI/Models/ExternalJobModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMRPublicAPI.Models
+{
+    public class ExternalJobModel
+    {
+        public long ExternalJobID { get; set; }
+        public string State { get; set; }
+        public string County { get; set; }
+        public int ReqJobOpeningId { get; set; }
+        public string JobRequirement { get; set; }
+    }
+}
