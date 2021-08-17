a6e8fbb4c49a32dc45956fcd436278429f7cf6cf
Md. Atikur Zaman <arzaman@ael-bd.com>
2021-08-17 14:03:26 +0600

    add log4 net

---
+++ UMRPublicAPI/Log.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using System;
+
+namespace UMRPublicAPI
+{
+    public class Log
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        internal static void Write(Exception ex)
+        {
+            string exceptionMessage = string.Empty;
+            if (ex.InnerException == null)
+            {
+                exceptionMessage = ex.Message;
+            }
+            else
+            {
+                exceptionMessage = ex.InnerException.Message;
+            }
+            log.Error("\n\nDateTime: " + DateTime.Now + " \n Message: " + exceptionMessage + "\nStackTrace: " + ex.StackTrace);
+        }
+        internal static void Info(Object info)
+        {
+            log.Info("\n\nDateTime: " + DateTime.Now + " \n Info Message: " + info);
+        }
+    }
+}
